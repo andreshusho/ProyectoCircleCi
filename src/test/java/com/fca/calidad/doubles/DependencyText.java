cc8c66b2e3647081632dc9a844e7e96147250f0f
Andres <ajer651@gmail.com>
2024-10-28 18:59:06 -0600

    Cambios a test doubles

--- src/test/java/com/fca/calidad/doubles/DependencyText.java
+++ src/test/java/com/fca/calidad/doubles/DependencyText.java
@@ -2,12 +2,13 @@ package com.fca.calidad.doubles;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 class DependencyText {
 	private Dependency dependency;
@@ -17,6 +18,22 @@ class DependencyText {
 	void setup() {
 		sub =mock(SubDependency.class);
 		dependency = new Dependency(sub);
+	}
+	
+	@Test
+	void addTwoTest() {
+		when(dependency.addTwo(anyInt())).thenAnswer(new Answer<Integer>() {
+			
+			public Integer answer(InvocationOnMock invocation) throws Throwable {
+				int arg = (Integer)invocation.getArguments()[0];
+				
+				return 20 * 2 + 10 + arg;
+			}
+		});
+		
+		int resultadoEsperado = 12;
+		
+		assertThat(resultadoEsperado, is(dependency.addTwo(5)));
 	}
 	
 	@Test
