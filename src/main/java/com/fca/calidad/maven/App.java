d57072e410a3f936fb9d3cbd9ab576892ed3a1ee
Andres <ajer651@gmail.com>
2024-11-20 19:24:39 -0600

    CAMBIOS 20/11/2024

--- src/main/java/com/fca/calidad/maven/App.java
+++ src/main/java/com/fca/calidad/maven/App.java
@@ -1,10 +1,25 @@
 package com.fca.calidad.maven;
 
+import com.fca.calidad.dao.DAOUserSQLite;
+import com.fca.calidad.model.User.User;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        DAOUserSQLite dao = new DAOUserSQLite();
+        User usuario = new User("nombre", "correo", "password");
+        int id = dao.save(usuario);
+        
+        usuario.setId(id);
+        
+        //find
+        System.out.println(dao.findById(id).toString());
+        
+        dao.deleteById(1);
+        
     }
 }
