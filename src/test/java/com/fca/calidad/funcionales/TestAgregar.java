ae75c0838f67a215ffd0ee01e588bd388350db7d
andreshusho <131018417+andreshusho@users.noreply.github.com>
2024-12-11 17:29:26 -0600

    Update TestAgregar.java

--- src/test/java/com/fca/calidad/funcionales/TestAgregar.java
+++ src/test/java/com/fca/calidad/funcionales/TestAgregar.java
@@ -60,7 +60,7 @@ public class TestAgregar {
     driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/form/button")).click();
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     WebElement confirmationMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[3]/div/div[2]/form/div[4]/div/div")));
-    assertEquals("Agregar", confirmationMessage.getText());
+    assertEquals("Nice one!", confirmationMessage.getText());
   }
     
   @After
